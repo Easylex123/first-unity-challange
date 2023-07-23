8f67a5a4e63020f0e947c5dfc94adec361a7cd1b
Lex <55781920+Easylex123@users.noreply.github.com>
2023-07-23 21:51:03 +0200

    downloaded discord rich presence and textmeshpro

--- JumpAndRunChallange/Assets/Code/Movement.cs
+++ JumpAndRunChallange/Assets/Code/Movement.cs
@@ -1,6 +1,7 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 namespace Code
@@ -10,43 +11,54 @@ namespace Code
         private const string HorizontalName = "Horizontal";
         private const string JumpName = "Jump";
         private const string SpeedParameterName = "Speed";
+        private const string JumpHeightParameterName = "IsJumping";
         private float _horizontalMove;
-        [SerializeField] private float _movementSpeed = 5f;
-        [SerializeField] private float _jumpingPower = 16f;
+        private Vector2 spawnpoint;
         private bool _isFacingRight = true;
 
-        [Space] [SerializeField] private Rigidbody2D rb;
+        [Space] [SerializeField] private Rigidbody2D player;
         [SerializeField] private Transform groundCheck;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Animator animator;
+        [SerializeField] private float _movementSpeed = 5f;
+        [SerializeField] private float _jumpingPower = 16f;
         private static readonly int Speed = Animator.StringToHash(SpeedParameterName);
+        private static readonly int IsJumpingName  = Animator.StringToHash(JumpHeightParameterName);
 
-
+        private void Start()
+        {
+            spawnpoint = transform.position;
+        }
+    
         // Update is called once per frame
         private void Update()
         {
+            animator.SetFloat("ySpeed", player.velocity.y);
             _horizontalMove = Input.GetAxisRaw(HorizontalName);
             
             animator.SetFloat(Speed, Mathf.Abs(_horizontalMove));
            
             if (Input.GetButtonDown(JumpName) && IsGrounded())
             {
-                rb.velocity = new Vector2(rb.velocity.x, _jumpingPower);
+                animator.SetBool(IsJumpingName, true);
+                player.velocity = new Vector2(player.velocity.x, _jumpingPower);
             }
 
-            if (Input.GetButtonUp(JumpName) && rb.velocity.y > 0f)
+            if (Input.GetButtonUp(JumpName) && player.velocity.y > 0f)
             {
-                var velocity = rb.velocity;
+                animator.SetBool(IsJumpingName, true);
+                var velocity = player.velocity;
                 velocity = new Vector2(velocity.x, velocity.y * 0.5f);
-                rb.velocity = velocity;
+                player.velocity = velocity;
             }
             
+            animator.SetBool(IsJumpingName, !IsGrounded());
             Flip();
         }
 
         private void FixedUpdate()
         {
-            rb.velocity = new Vector2(_horizontalMove * _movementSpeed, rb.velocity.y);
+            player.velocity = new Vector2(_horizontalMove * _movementSpeed, player.velocity.y);
         }
 
         private void Flip()
@@ -62,6 +74,14 @@ namespace Code
         private bool IsGrounded()
         {
             return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.tag.Equals("FallDetector"))
+            {
+                player.position = spawnpoint;
+            }
         }
     }
 }
